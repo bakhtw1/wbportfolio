e650b38aee2c4dc2fea50646ce5dc031ef10abf3
bakhtw1 <bakhtw1@gmail.com>
2019-04-10 00:39:03 -0400

    Inital Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// C library API
+const ffi = require('ffi');
+
+// Express App (Routes)
+const express = require("express");
+const app     = express();
+const path    = require("path");
+const fileUpload = require('express-fileupload');
+const mysql = require('mysql');
+
