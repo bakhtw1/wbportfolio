3bd3074ae8a87694a706e6ef973e4324a33702ff
bakhtw1 <bakhtw1@gmail.com>
2019-04-10 01:15:02 -0400

    Updated Public Files

--- app.js
+++ app.js
@@ -8,9 +8,28 @@ const express = require("express");
 const app     = express();
 const path    = require("path");
 const fileUpload = require('express-fileupload');
-const mysql = require('mysql');
 
+const portNum = process.argv[2];
+
+// Send HTML at root, do not change
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
+  
+  // Send Style, do not change
+app.get('/style.css',function(req,res){
+    //Feel free to change the contents of style.css to prettify your Web app
+    res.sendFile(path.join(__dirname+'/public/style.css'));
+});
+  
+  // Send obfuscated JS, do not change
+app.get('/index.js',function(req,res){
+    fs.readFile(path.join(__dirname+'/public/index.js'), 'utf8', function(err, contents) {
+        const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {compact: true, controlFlowFlattening: true});
+        res.contentType('application/javascript');
+        res.send(minimizedContents._obfuscatedCode);
+    });
+});
 
+app.listen(portNum);
+console.log('Running app at localhost: ' + portNum);
